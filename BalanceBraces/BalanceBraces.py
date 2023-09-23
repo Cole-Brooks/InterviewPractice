a717dc850d9e182fa575832d288020ed1285368a
Cole Brooks <cole.brooks@hotmail.com>
2023-09-23 15:06:00 -0500

    finished balanced braces

--- BalanceBraces/BalanceBraces.py
+++ BalanceBraces/BalanceBraces.py
@@ -1,19 +1,41 @@
-class BraceBalancer:
+class BalanceBraces:
     def __init__(self):
         self.braces = ['[', '{', '(', ')', '}', ']']
         self.stack = []
 
-    def doBracesMatch(self, braceA, braceB):
+    def clean_stack(self):
+        self.stack = []
+
+    def do_braces_match(self, braceA, braceB):
         if braceA == '[' and braceB == ']' or braceA == '{' and braceB == '}' or braceA == '(' and braceB == ')':
             return True
         return False
 
-    def areBracesBalanced(self, braces):
-        for brace in [*braces]:
+    def massage_inputs(self, braces):
+        '''
+        Takes a string 'braces' as input. Outputs a list of brace characters. Other characters are not
+        looked at when we're determining if braces are balanced.
+        '''
+        return [brace for brace in [*braces] if brace in self.braces]
+
+
+    def are_braces_balanced(self, braces):
+        for brace in self.massage_inputs(braces):
             if not self.stack:
                 self.stack.append(brace)
-            elif self.doBracesMatch(self.stack[-1], brace):
+            elif self.do_braces_match(self.stack[-1], brace):
                 _ = self.stack.pop()
             else:
                 self.stack.append(brace)
-        return (not self.stack)
+        balanced = not self.stack
+        self.clean_stack()
+        return (balanced)
+    
+def test(test_string):
+    braceBalancer = BalanceBraces()
+    print(test_string)
+    print(braceBalancer.are_braces_balanced(test_string))
+
+if __name__=="__main__":
+    test_string = "[({[()()]})]()()[({[()()]})]()()[({[()()]})]()()[({[()()]})]()"
+    test(test_string)
